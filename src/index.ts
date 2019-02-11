102bcc718bf593eaf1f80db6787cc43ff28c235f
Hendrik Lammers <lammers@gmail.com>
2019-02-11 21:36:24 +0700

    Add htmlparser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import htmlparser from 'htmlparser2'
 
+const parser = new htmlparser.Parser({
+  onopentag: (name, attribs) => {
+    console.log('name', name)
+    console.log('attribs', attribs)
+  },
+})
+
+parser.write('<div class="foo"><span>Hello world</span></div>')
+parser.end()
