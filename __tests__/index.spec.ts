f941ded5c7264869925c35de1b58d2e467b9d7d0
Hendrik Lammers <lammers@gmail.com>
2019-02-13 17:09:46 +0700

    Update tests

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,12 +1,44 @@
-import { parseHtml } from '../src/index'
+import { convert } from '../src/index'
 
-describe('parseHtml function', () => {
+describe('convert function', () => {
   it('converts a single html element string into elm-html', () => {
     const html = '<div>Hello world</div>'
     const elm = `div
     []
     [ text "Hello world" ]`
 
-    expect(parseHtml(html)).toBe(elm)
+    expect(convert(html)).toBe(elm)
+  })
+
+  it('trims whitespace', () => {
+    const html = `<div>
+        Hello world   </div>`
+    const elm = `div
+    []
+    [ text "Hello world" ]`
+    expect(convert(html)).toBe(elm)
+  })
+
+  it('works with nested tags', () => {
+    const html = '<div><span>Hello world</span><span>second</span></div>'
+    const elm = `div
+    []
+    [ span
+        []
+        [ text "Hello world" ]
+    , span
+        []
+        [ text "second" ]
+    ]`
+
+    expect(convert(html)).toBe(elm)
+  })
+
+  it('parses the attributes', () => {
+    const html = `<input class="form-control" type="text" placeholder="Search" readonly>`
+    const elm = `input
+    [ class "form-control", type_ "text", placeholder "Search", readonly True ]
+    []`
+    expect(convert(html)).toBe(elm)
   })
 })
